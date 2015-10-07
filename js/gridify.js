0092348276b501a8901d84168a083955faf118e8
mlmedia <millionlightsmedia@gmail.com>
2015-10-06 21:54:58 -0400

    - just beautifying

--- js/gridify.js
+++ js/gridify.js
@@ -1,33 +1,27 @@
-
 /* define $ as jQuery just in case */
-( function( $ )
-{
+(function($) {
 	/* doc ready */
-	$( function( )
-	{
-		var $list = $( '.grid' );
-		var $items = $list.find( '.item' );
-		var setHeights = function()
-		{
-			$items.css( 'height', 'auto' );
-			var perRow = Math.floor( $list.width() / $items.width() );
-			if( perRow == null || perRow < 2 ) return true;
-			for( var i = 0, j = $items.length; i < j; i += perRow )
-			{
-				var maxHeight   = 0,
-				$row = $items.slice( i, i + perRow );
+	$(function() {
+		var $list = $('.grid');
+		var $items = $list.find('.item');
+		var setHeights = function() {
+			$items.css('height', 'auto');
+			var perRow = Math.floor($list.width() / $items.width());
+			if (perRow == null || perRow < 2) return true;
+			for (var i = 0, j = $items.length; i < j; i += perRow) {
+				var maxHeight = 0,
+					$row = $items.slice(i, i + perRow);
 
-				$row.each( function()
-				{
-					var itemHeight = parseInt( $( this ).outerHeight() );
-					if ( itemHeight > maxHeight ) maxHeight = itemHeight;
+				$row.each(function() {
+					var itemHeight = parseInt($(this).outerHeight());
+					if (itemHeight > maxHeight) maxHeight = itemHeight;
 				});
-				$row.css( 'height', maxHeight );
+				$row.css('height', maxHeight);
 			}
 		};
 		setHeights();
-		$( window ).on( 'load', setHeights );
-		$( window ).on( 'resize', setHeights );
+		$(window).on('load', setHeights);
+		$(window).on('resize', setHeights);
 
 		/* lazy loading */
 		/*
@@ -38,4 +32,4 @@
 
 	});
 
-})( jQuery );
+})(jQuery);
